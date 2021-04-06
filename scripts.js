1c71337b8834d18bc52cc0c7ce4c4ef901403d04
Yan Felix <yandacruzfelix@gmail.com>
2021-04-05 22:31:51 -0300

    Sixth Commit

---
+++ scripts.js
@@ -0,0 +1,29 @@
+const Modal = {
+    open(){
+        document
+            .querySelector('.Recipes-container')
+            .classList
+            .add('actived')
+
+        document
+            .querySelector('.backgroud-recipes')
+            .classList
+            .add('actived')
+    },
+
+    close(){
+        document
+            .querySelector('.Recipes-container')
+            .classList
+            .remove('actived')
+
+        document
+            .querySelector('.backgroud-recipes')
+            .classList
+            .remove('actived')
+    }
+}
+
+const EnterPage = {
+    
+}
