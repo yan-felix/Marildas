81dc096d23c028b2a06fa22fa916fc40a451ef1e
Yan Felix <yandacruzfelix@gmail.com>
2021-04-20 22:53:00 -0300

    Eighth Commit

--- scripts.js
+++ scripts.js
@@ -1,25 +1,57 @@
+const GetDatasPost = {
+    getCodsPost(){
+        // Pegar os códigos (txt[n] e img[n]) do post e usá-lo para pegar o texto(receita) e a imagem correspondentes a estes códigos.
+    }
+}
+
+const PostedRecipesVisualization = {
+    ToShowRecipe(){
+        let p = document.createElement('p');
+        document.querySelector('#p-container').appendChild(p);
+
+        let img = document.createElement('img');
+     /* img.setAttribute("src", "*endereço da imagem*"); */
+        img.setAttribute("alt", '"Imagem da Receita Pronta"');
+        document.querySelector('#Recipe-Image-container').appendChild(img);
+    },
+
+    ToHideRecipe(){
+        document.querySelector('#p-container').remove('p');
+
+        document.querySelector('#Recipe-Image-container').removeChild('img');
+    },
+    
+}
+
 const Modal = {
     open(){
         document
-            .querySelector('.Recipes-container')
+            .querySelector('.Recipe-container')
             .classList
-            .add('actived')
+            .add('actived');
 
         document
-            .querySelector('.backgroud-recipes')
+            .querySelector('.backgroud-recipe-container')
             .classList
-            .add('actived')
+            .add('actived');
+
+        PostedRecipesVisualization.ToShowRecipe();
     },
 
     close(){
         document
-            .querySelector('.Recipes-container')
+            .querySelector('.Recipe-container')
             .classList
-            .remove('actived')
+            .remove('actived');
 
         document
-            .querySelector('.backgroud-recipes')
+            .querySelector('.backgroud-recipe-container')
             .classList
-            .remove('actived')
-    }
+            .remove('actived');
+
+        PostedRecipesVisualization.ToHideRecipe();
+    },
+
 }
+
+
