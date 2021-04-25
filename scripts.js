f940ba1991c3d8d0616267103a41300c0926bd1d
Yan Felix <yandacruzfelix@gmail.com>
2021-04-25 19:32:40 -0300

    Twelfth Commit

--- scripts.js
+++ scripts.js
@@ -4,9 +4,13 @@ const PostedRecipes = [
         name: `Filé de Peixe`,
         text: `
             - 1 filé de peixe grelhado;
+            <br>
             - Uns pedaços de couve flor empanada no ovo e queijo ralado;
+            <br>
             - Corte meio pimentão e rechei com lentilha cozida e refogada;
+            <br>
             - Coloque no forno por 10 minutos;
+            <br>
             - Faça o molho com mostarda, creme de leite, sal e manteiga. Leve ao fogo os ingredientes até ferver e coloque em cima do peixe.
         `,
         img: `./assets/assets/file-de-peixe.png`,
@@ -17,6 +21,7 @@ const PostedRecipes = [
         name: `Camarão com Legumes`,
         text: `
             - Coloque para refogar abobrinha e tomate miúdo  no alho e cebola. Sal e pimenta do reino no arroz selvagem cozido normalmemte. Refogue 4 camarões grandes até ficarem vermelhos;
+            <br>
             - No meio do prato, desenforme o arroz. Coloque os legumes em volta do arroz e decore com os 4 camarões com o rabinho para cima. Decore com salsinha picada, e o azeite do regogado do camarão jogue por cima
         `,
         img: `./assets/assets/camarao-com-legumes.png`,
@@ -27,31 +32,46 @@ const PostedRecipes = [
         name: `Salada com Queijo de Cabra`,
         text: `
             #Ingradientes:
+            <br>
                 -1 maçã verde;
+                <br>
                 -1cenoura;
+                <br>
                 -100g de amenda;
+                <br>
                 -1 ramo de salsicha picada;
+                <br>
                 -4 tomates miúdo;
+                <br>
                 -1 fatia de pão de forma;
+                <br>
                 -2 rodelas de queijo de quebra(mussarela);
+                <br>
                 -1 colher de mel;
+                <br>
                 -2 colher de mostarda;
+                <br>
                 -2 colher de manteiga;
+                <br>
                 -250 ml de cheme de leite;
-                -Sal (a gosto); 
+                <br>
+                -Sal (a gosto);
+                <br> 
                 -Pimenta do Reino;
+                <br>
                 -1 colher de molho inglês;
+                <br>
                 -Um punhado de orégano.
-
+            <br>
             #Preparo do Molho:
                 2 colheres de manteiga para derreter, um colher de mostarda, uma colher de mel tudo para derreter na panela, depois que derreter coloca um pouco de orégano e 250 ml de creme de leite, quando tiver abrir fervura, coloca meia colher de molho inglês, deixa ferver e só coloca na sala quando o molho estiver frio.
-
+            <br>
             #Como Fazer:
                 Corte a cenoura em lâminas bem finas, as maçãs em lâminas bem finas com casca, amêndoas socas com o socador levemente, os dois pões cortados em triângulo e posto no forno para asar.
 
                 Depois pegue a salada  com todos os ingredientes tempere com o molho e põem dentro do prato e coloque as fatias de pão nas laterais, com a rodela de queijo por cima.
 
-                Obs: pôr por cima, os ingredientes que contém na salada, para decoração.
+                [Obs: pôr por cima, os ingredientes que contém na salada, para decoração].
                 
                 Finalize com salsicha picada.
         `,
@@ -63,12 +83,17 @@ const PostedRecipes = [
         name: `Salada Primavera`,
         text: `
             #Ingredientes:
+            <br>
                 - 3 morangos;
+                <br>
                 - 2 kiws;
+                <br>
                 - 1 manga;
+                <br>
                 - Algumas frutinhas vermelhas.
-            
+            <br>
             #Como Fazer:
+            <br>
                 Corte tudo em fatias. Faça a decoração em forma de flor com as frutas vermelhas na lateral e coloque um galho de hortelã em cima.
         `,
         img: `./assets/assets/salada-primavera.png`,
@@ -79,6 +104,7 @@ const PostedRecipes = [
         name: `Ravióli com Paito de Frango`,
         text: `
             - Cozinhe o ravióli por  4 minutos e refoque um peito de frango cortado ao meio; dore bem dos dois lados até ficar bem cozido.
+            <br>
             - Molho: dore uma cebola picadinha na manteiga. Jogue creme de leite, um pouco de curry vermelho, deixe ferver e jogue em cima
         `,
         img: `./assets/assets/ravioli-com-peito-de-frango.png`,
@@ -89,17 +115,22 @@ const PostedRecipes = [
         name: `Pudim de Laranja com Calda`,
         text: `
             #Ingredientes:
-                - 1 lata de Leite Condensado.
-                - 1 lata de Suco de Laranja.
+            <br>
+                - 1 lata de Leite Condensado;
+                <br>
+                - 1 lata de Suco de Laranja;
+                <br>
                 - 4 Ovos.
-
+            <br>
             #Para a Calda:
+            <br>
                 Coloque 1 lata de caldo de laranja e 1/2 copo de Açúcar.
                 Ponha para fever até engrossar.
                 Pique umas cascas de Laranjas bem finas.
                 Escorra e ferva junto com a calda e coloque em cima do pudim para decorar.
-
+            <br>
             #Como Fazer:
+            <br>
                 Coloque, suco, os ovos e o leite condensado no liquidificador. 
                 Bata uns 5 minutos, coloque em forminhas para assar, em Banho Maria por uns 30 minutos. Testando com um palito, até o palito sair limpo do Pudim.
         `,
@@ -122,29 +153,61 @@ const PostedRecipes = [
 
 ]
 
+let QuantAccounts = [0] //Essa merda provavelmente será inútil.
+
+const Storege = {
+    get() {
+        return JSON.parse(localStorage.getItem("AccountDatas:")) || {}
+    },
+
+    set(Account) {
+        localStorage.setItem("AccountDatas:", JSON.stringify(Account));
+    }
+}
+
 const ShowRecipe = {
     recipes: PostedRecipes,
 
     getPost(index){
-        let Recipe = ShowRecipe.recipes[`${index}`];
+        const Recipe = ShowRecipe.recipes[`${index}`];
+        let H3html = `${Recipe.name}`;
+        let Phtml = `
+                ${Recipe.text}
+        `;
+
+        let h3 = document.createElement('h3');
+        h3.setAttribute("id", "h3-recipe");
+        h3.innerHTML = H3html;
+        document.querySelector("#name-recipe-container").appendChild(h3);
 
         let p = document.createElement('p');
-        p = `<p>${Recipe.text}</p>`;
+        p.setAttribute("id", "p-recipe");
+        p.innerHTML = Phtml;
+        document.querySelector("#p-container").appendChild(p);
         console.log(p);
 
-        let img = document.querySelector('#Recipe-Image');
+        const img = document.querySelector('#Recipe-Image');
         img.setAttribute("src", `${Recipe.img}`);
         img.setAttribute("alt", `Imagem: ${Recipe.name}`);
         
         index = "";
     },
 
-    hideRecipe(){
+    hidePost(){
+        let nodeH3 = document.getElementById("h3-recipe");
+        if (nodeH3.parentNode){
+            nodeH3.parentNode.removeChild(nodeH3);
+        }
 
-        let img = document.querySelector('#Recipe-Image');
+        let nodeP = document.getElementById("p-recipe");
+        if (nodeP.parentNode){
+            nodeP.parentNode.removeChild(nodeP);
+        }
+
+        const img = document.querySelector('#Recipe-Image');
         img.setAttribute('src', '');
         img.setAttribute('alt', '');
-    },
+    }
     
 }
 
@@ -174,7 +237,7 @@ const Modal = {
             .classList
             .remove('actived');
 
-        ShowRecipe.hideRecipe();
+        ShowRecipe.hidePost();
     },
 
     openSubscribeForm(){
@@ -202,5 +265,30 @@ const Modal = {
     },
 
 }
+
+
+const CreateAccount = {
+    Name: document.querySelector("#SubscribeName"),
+    Password: document.querySelector("#SubscribePassword"),
+    UserCod: `M${QuantAccounts.length + 1}U`, // Fazer esse maldito gerador de código funcionar.
+    NPosts: 0,
+
+    RegisterNewAccount(){
+        let Account = {
+            name: CreateAccount.Name.value,
+            password: CreateAccount.Password.value,
+            userCod: CreateAccount.UserCod,
+            nPosts: CreateAccount.NPosts,
+        }
+
+        Storege.set(Account);
+        QuantAccounts.push(QuantAccounts.length);
+        Modal.closeSubscribeForm();
+
+    },
+
+    
+}
+
 
 
