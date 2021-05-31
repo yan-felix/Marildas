0a1b095401fbfb91fb18e00253dec488b8dada7c
Yan Felix <yandacruzfelix@gmail.com>
2021-05-31 18:01:18 -0300

    Nineteenth Commit

--- scripts.js
+++ scripts.js
@@ -73,15 +73,42 @@ const PostedRecipes = [
             <p>
             #Ingredientes:
             <br>
-                -
-
+                - 2,0 filés médios de Peixe salmão cru;
+                <br>
+                - 1,0 unidade média de Laranja pera;
+                <br>
+                - Orégano seco à gosto;
+                <br>
+                - 2,0 folhas de Sálvia fresca;
+                <br>
+                - Sal refinado à gosto;
+                <br>
+                - 1,0 colher (chá) de Óleo de soja Soya;
+                <br>
+                - 1,0 unidade pequena de Alface crespa;
+                <br>
+                - 2,0 folhas grandes de Acelga;
+                <br>
+                - 10,0 folhas de Rúcula;
+                <br>
+                - 1,0 unidade pequena de Tomate;
+                <br>
+                - 4,0 colheres (sopa) de Vagem cozida;
+                <br>
+                - 0,5 unidades médias de Pepino;
+                <br>
+                - 2,0 colheres (sopa) picadas de Cebola;
+                <br>
+                - 2,0 colheres (sopa) de Vinagre balsâmico Spoleto;
+                <br>
+                - 1,0 colher (sopa) de Salsinha;
+                <br>
+                - 2,0 colheres (sopa) de Semente de linhaça.
             </p>
             <p>
             #Como fazer:
             <br>
-                - Cozinhe o ravióli por  4 minutos e refoque um peito de frango cortado ao meio; dore bem dos dois lados até ficar bem cozido;
-                <br>
-                - Molho: dore uma cebola picadinha na manteiga. Jogue creme de leite, um pouco de curry vermelho, deixe ferver e jogue em cima.
+                - Tempere os filés de salmão com o sal, o sumo da laranja, o orégano e a sálvia. Aqueça o óleo em uma frigideira, e doure os filés de salmão. Corte os filés em tiras e reserve. Lave as folhas de acelga e pique em tiras finas. Em uma saladeira disponhas folhas de alface, rúcula, acelga picada e a vagem cozida. Pique o tomate, o pepino e a cebola em cubos e acrescente sobre as folhas. Junte as tiras de salmão e misture delicadamente. Espalhe o vinagre sobre as folhas e salpique a salsinha e a linhaça. Sirva.
         `,
         img: `./assets/assets/salmao-cru.png`
     },
@@ -105,10 +132,13 @@ const PostedRecipes = [
             <p>
             #Para a Calda:
             <br>
-                Coloque 1 lata de caldo de laranja e 1/2 copo de Açúcar.
-                Ponha para fever até engrossar.
-                Pique umas cascas de Laranjas bem finas.
-                Escorra e ferva junto com a calda e coloque em cima do pudim para decorar.
+                - Coloque 1 lata de caldo de laranja e 1/2 copo de Açúcar;
+                <br>
+                - Ponha para fever até engrossar;
+                <br>
+                - Pique umas cascas de Laranjas bem finas;
+                <br>
+                - Escorra e ferva junto com a calda e coloque em cima do pudim para decorar.
             </p>
             <p>
             #Como Fazer:
@@ -244,7 +274,6 @@ const PostedRecipes = [
                 - 1 xícara (chá) de farinha de trigo peneirada;
                 <br>
                 - 8 bolas de Sorvete Nestlé Creme Tradicional.
-
             </p>
             <p>
             #Como fazer:
@@ -273,13 +302,25 @@ const PostedRecipes = [
             <p>
             #Ingredientes:
             <br>
-                -
+                - 400 g de ravióli de frango;
                 <br>
-                -
+                - 2 colheres (sopa) de manteiga;
                 <br>
-                -
+                - 2 colheres (café) de noz moscada;
                 <br>
-                -
+                - Queijo parmesão ralado;
+                <br>
+                - Salsinha;
+                <br>
+                - Requeijão cremoso;
+                <br>
+                - 2 sachês de caldo de frango;
+                <br>
+                - 3 tomates;
+                <br>
+                - 2 dentes de alho;
+                <br>
+                - 1 cebola picadinha.
                 <br>
             </p>
             <p>
@@ -328,7 +369,7 @@ const PostedRecipes = [
                 <br>
                 - 1/2 lata de leite condensado;
                 <br>
-                - Suco de 2 limões;
+                - Suco de 2 limões.
             </p>
             <p>
             #Como Fazer:
@@ -382,6 +423,138 @@ const PostedRecipes = [
             `,
         img: `./assets/assets/foto-de-perfil-oficial-temporaria_files/salmão-ao-molho-de-limão.jpg`,
     },
+]
+
+const MemberPosts = [
+    {
+        index: 0,
+        nameUser: 'Mariana Termanye',
+        photoProfile: './assets/assets/Mariana-foto-perfil.png',
+        nameRecipe: 'Feijoada Vegana',
+        text: `
+        <p id="MemberPsotPresentation">
+        Tinha umas carnes vegetais diferentes dando sopa no congelador, resolvi fazer uma feijoada também! Próxima a original, ao menos em aspecto e textura. O segredo também está no tempero, em usar fumaça em pó ou líquida, fica show. Se quiser só usar a soja marinada pra baratear a receita, fica uma delícia também. Nesse caso coloco umas cenouras também, e não esqueça da fumaça (líquida ou em pó).
+        </p>
+        <p>
+        #Ingredientes:
+            - 250 g feijão preto demolhado;
+            <br>
+            - 1 litro água para cozinhar o feijão;
+            <br>
+            - 150 g frango vegetal (usei o "vrango" da Pop Vegan);
+            <br>
+            - 3 linguiças vegetais (usei da Vegabom);
+            <br>
+            - 100 g soja texturizada hidratada, da maior;
+            <br>
+            - 2 colheres de sopa páprica defumada;
+            <br>
+            - 1 colher de sopa fumaça em pó;
+            <br>
+            - 1 colher de chá alho em pó;
+            <br>
+            - 2 colheres de chá mistura de ervas para chimichurri;
+            <br>
+            - 2 limões, o suco;
+            <br>
+            - 3 colheres de sopa molho shoyu;
+            <br>
+            - 2 colheres de sopa azeite;
+            <br>
+            - 1 cebola grande picada;
+            <br>
+            - 5 dentes alho amassados;
+            <br>
+            - 1 colher de sopa óleo de girassol ou outro vegetal;
+            <br>
+            - Pimenta-do-reino (a gosto);
+            <br>
+            - Folhas de louro (a gosto);
+            <br>
+            - 1 colher de café sal se precisar.
+        </p>
+        <p>
+        #Como fazer:
+        <br>
+            - Começo a fazer a feijoada 1 noite antes, deixando o feijão de molho. Também deixo a soja de molho por 20 minutos em água quente, escorro e faço um marinado. Coloco páprica, fumaça em pó (ou líquida), alho em pó, chimichurri, limão e azeite. Tampo o marinado de soja e só vou abrir no dia seguinte, na hora de refogar;
+            <br>
+            - No dia seguinte, coloque o feijão para cozinhar, cubra com água, leve ao fogo alto com a tampa semiaberta, quando a água ferver, baixe o fogo e vede a panela. Deixo cozinhar por uns 25 minutos. Reserve;
+            <br>
+            - Pra essa fejuca, resolvi tentar fazer o mais próximo do original possível, usando substitutos vegetais de carnes diferentes, pra criar a textura e o gosto. Mas você pode usar vegetais no lugar, talvez só a soja pra baratear, fica bem boa também. Nesse caso faz toda a diferença colocar a fumaça em pó ou líquida na soja. Enfim, corte as carnes vegetais e reserve;
+            <br>
+            - Leve o óleo e a cebola picada a panela, refogue em fogo baixo. Depois adicione a soja marinada e o alho, refogue bem, por uns 3 minutos;
+            <br>
+            - Acrescente o "vrango" e a linguiça de soja, refogue novamente e misture por mais 2-3 minutos;
+            <br>
+            - Acrescente o feijão cozido, o sal, a pimenta-do-reino e as folhas de louro. Feche a panela e deixe apurar por uns 5 minutos em fogo baixo;
+            <br>
+            - Sirva com os acompanhamentos favoritos e seja feliz!
+        </p>`,
+        img: './assets/assets/feijoada-vegana.png',
+    },
+    
+    {
+        index: 1,
+        nameUser: 'Ricardo Yagger',
+        photoProfile: './assets/assets/Ricardo-foto-perfil.png',
+        nameRecipe: 'Cheesecake de Maracujá e Chocolate Branco',
+        text: `
+        <p id="MemberPsotPresentation">
+        Cheesecake é uma sobremesa democrática: se adapta aos mais variados sabores e conquista muita gente na primeira garfada. Essa é feita com uma combinação infalível – maracujá e chocolate branco. O melhor de tudo é que ela não vai ao forno e é muito fácil de fazer!
+        </p>
+        <p>
+        #Ingredientes:
+        <br>
+            - 200 gramas de biscoito sem recheio;
+            <br>
+            - 100 gramas de coco ralado seco e tostado;
+            <br>
+            - 100 gramas de manteiga sem sal;
+            <br>
+            - 3/4 de xícara de suco de maracujá (cerca de 3 unidades);
+            <br>
+            - 600 gramas de cream cheese;
+            <br>
+            - 1/3 de xícara de açúcar;
+            <br>
+            - 400 gramas de chocolate branco;
+            <br>
+            - 1 caixa de creme de leite;
+            <br>
+            - Polpa de 2 maracujás;
+            <br>
+            1/2 colher de sopa de açúcar;
+        </p>
+        <p>
+        #Como fazer:
+        <br>
+            - Processe os biscoitos com o coco ralado. Em seguida, despeje a manteiga derretida sobre essa mistura e processe novamente;
+            <br>
+            - Forre o fundo e as laterais de uma forma de 21 cm de diâmetro (com fundo removível) com essa farofa úmida e reserve na geladeira;
+            <br>
+            - Bata a polpa dos maracujás no liquidificador e espere alguns minutos para que as sementes se depositem no fundo. Depois, coe o suco - você vai precisar de 3/4 de xícara;
+            <br>
+            - No processador, junte o suco do maracujá, o açúcar e o cream cheese. Processe;
+            <br>
+            - Despeje o chocolate branco derretido com o creme de leite sobre a mistura e processe até obter um creme homogêneo;
+            <br>
+            - Transfira o creme para a forma, despejando delicadamente sobre a base de biscoitos. Alise a superfície com uma espátula e leve à geladeira por 4 horas;
+            <br>
+            - Junte a polpa do maracujá e o açúcar e leve para esquentar em uma panelinha até que a calda fique mais líquida;
+            <br>
+            - Despeje a calda sobre a cheesecake na hora de servir e aproveite!
+        </p>
+        `,
+        img: './assets/assets/cheesecake-de-maracujá-e-chocolate-branco.png',
+    },
+    
+    {
+        index: 2,
+        name: '',
+        text: '',
+        img: '',
+    },
+
 ]
 
 const Storege = {
@@ -541,6 +714,80 @@ const ShowRecipe = {
     },
 }
 
+const ShowMemberPostRecipe = {
+    recipes: MemberPosts,
+
+    openBook(index){
+        document
+            .querySelector('.Recipe-container')
+            .classList
+            .add('actived');
+
+        document
+            .querySelector('.backgroud-recipe-container')
+            .classList
+            .add('actived');
+
+        ShowMemberPostRecipe.getPost(index);
+    },
+
+    getPost(index){
+        const Recipe = ShowMemberPostRecipe.recipes[`${index}`];
+        let H3html = `${Recipe.name}`;
+        let Phtml = `
+                ${Recipe.text}
+        `;
+
+        let h3 = document.createElement('h3');
+        h3.setAttribute("id", "h3-recipe");
+        h3.innerHTML = H3html;
+        document.querySelector("#name-recipe-container").appendChild(h3);
+
+        let p = document.createElement('p');
+        p.setAttribute("id", "p-recipe");
+        p.innerHTML = Phtml;
+        document.querySelector("#p-container").appendChild(p);
+        console.log(p);
+
+        const img = document.querySelector('#Recipe-Image');
+        img.setAttribute("src", `${Recipe.img}`);
+        img.setAttribute("alt", `Imagem: ${Recipe.name}`);
+        console.log(img);
+        
+        index = "";
+    },
+
+    hidePost(){
+        let nodeH3 = document.getElementById("h3-recipe");
+        if (nodeH3.parentNode){
+            nodeH3.parentNode.removeChild(nodeH3);
+        }
+
+        let nodeP = document.getElementById("p-recipe");
+        if (nodeP.parentNode){
+            nodeP.parentNode.removeChild(nodeP);
+        }
+
+        const img = document.querySelector('#Recipe-Image');
+        img.setAttribute('src', '');
+        img.setAttribute('alt', '');
+    },
+    
+    closeBook(){
+        document
+            .querySelector('.Recipe-container')
+            .classList
+            .remove('actived');
+
+        document
+            .querySelector('.backgroud-recipe-container')
+            .classList
+            .remove('actived');
+
+        ShowRecipe.hidePost();
+    },
+}
+
 const ShowComments = {
     recipes: PostedRecipes,
 
@@ -603,29 +850,52 @@ const ShowComments = {
     }
 }
 
-const NewPost = {
-    openNewPostForm(){
-        document
-            .querySelector('.Do-New-Post-container')
-            .classList
-            .add('actived');
+const ShowCommentsMemberPost = {
+    recipes: MemberPosts,
 
-        document
-            .querySelector('.backgroud-do-new-post-container')
-            .classList
-            .add('actived');
+    getPostDatas(index){
+        const Recipe = ShowCommentsMemberPost.recipes[`${index}`];
+
+        let UserName = document.getElementById('NameMemberContainer');
+        UserName.innerText = `${Recipe.nameUser}`;
+
+        let UserPhoto = document.getElementById('PhotoMemberContainer');
+        UserPhoto.innerHTML =  `
+            <img src="${Recipe.photoProfile}" alt="${UserName}">
+        `;
+
+        let RecipeName = `${Recipe.nameRecipe}`;
+
+        let RecipeText = `
+                ${Recipe.text}
+        `;
+
+        let RecipeImage = `${Recipe.img}`;
+
+        let div1 = document.getElementById('CommentedMemberPostImgContainer');
+        div1.innerHTML = `
+            <img src="${RecipeImage}" alt="${RecipeName}">
+        `
+
+        let h2 = document.getElementById('CommentedMemberPostName');
+        h2.innerText = RecipeName;
+
+        let div2 = document.getElementById('CommentedMemberPostTextCotainer');
+        div2.innerHTML = `${RecipeText}`;
     },
 
-    closeNewPostForm(){
-        document
-            .querySelector('.Do-New-Post-container')
-            .classList
-            .remove('actived');
+    openCommentBox(index){
+        document.getElementById('PostsCommunityContainer').classList.add('hidden');
+        
+        document.getElementById(`CommentsBoxMemberPost`).classList.remove('hidden');
 
-        document
-            .querySelector('.backgroud-do-new-post-container')
-            .classList
-            .remove('actived');
+        ShowCommentsMemberPost.getPostDatas(index);
+    },
+
+    closeCommentBox(){
+        document.getElementById('PostsCommunityContainer').classList.remove('hidden');
+        
+        document.getElementById(`CommentsBoxMemberPost`).classList.add('hidden');
     }
 }
 
@@ -696,6 +966,48 @@ function deslikePost(PostNumber){
         </svg>`;
 
     let likedHeart = document.getElementById(`${PostNumber}`);
+
+    let div = likedHeart.parentNode;
+
+    div.removeChild(likedHeart);
+    div.appendChild(unLikedHeart);
+}
+
+function likeMemberPost(){
+    let likedHeart = document.createElement('a');
+    likedHeart.setAttribute('id', `LikeMemberPostIndex0`);
+    likedHeart.setAttribute('class', 'd-inline-block liked animate__heartBeat');
+    likedHeart.setAttribute('href', 'javascript:void(0)');
+    likedHeart.setAttribute('onclick', `deslikeMemberPost('LikeMemberPostIndex0')`);
+    likedHeart.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+            class="bi bi-suit-heart" viewBox="0 0 16 16">
+            <path
+                d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z" />
+        </svg>`;
+
+    let unLikedHeart = document.getElementById(`LikeMemberPostIndex0`);
+
+    let div = unLikedHeart.parentNode;
+
+    div.removeChild(unLikedHeart);
+    div.appendChild(likedHeart);
+}
+
+function deslikeMemberPost(){
+    let unLikedHeart = document.createElement('a');
+    unLikedHeart.setAttribute('id', `LikeMemberPostIndex0`);
+    unLikedHeart.setAttribute('class', 'link-like d-inline-block');
+    unLikedHeart.setAttribute('href', 'javascript:void(0)');
+    unLikedHeart.setAttribute('onclick', `likeMemberPost('LikeMemberPostIndex0')`);
+    unLikedHeart.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+            class="bi bi-suit-heart" viewBox="0 0 16 16">
+            <path
+                d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z" />
+        </svg>`;
+
+    let likedHeart = document.getElementById(`LikeMemberPostIndex0`);
 
     let div = likedHeart.parentNode;
 
